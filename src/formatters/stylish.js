d2d72151ce5662b6228a012fe3ed93dfebbbaf4d
zluuba <zluyba.nikitina@gmail.com>
2023-07-14 15:10:44 +0300

    feat: add stylish formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,10 +6,38 @@ const flags = {
   delete: ' - ',
 };
 
-const getStylishFormatDiff = (diff) => {
-  const stylishDiff = {};
+const getStylishFormatDiff = (diff, replacer = ' ', spacesCount = 1) => {
+  const iter = (node, depth) => {
+    if (!_.isObject(node)) {
+      return `${node}`;
+    }
 
-  return diff;
+    const indentSize = depth * spacesCount;
+    const currentIndent = replacer.repeat(indentSize);
+    const bracketIndent = replacer.repeat(indentSize - spacesCount);
+
+    const lines = [];
+
+    Object
+      .entries(node)
+      .map(([key, val]) => {
+        if (val.flag === 'update') {
+          lines.push(`${currentIndent}${flags.delete}${key}: ${iter(val.value[0], depth + 1)}`);
+          lines.push(`${currentIndent}${flags.add}${key}: ${iter(val.value[1], depth + 1)}`);
+          return;
+        }
+        lines.push(`${currentIndent}${flags[val.flag]}${key}: ${iter(val.value, depth + 1)}`);
+        return;
+      })
+
+    return [
+      '{',
+      ...lines,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+
+  return iter(diff, 1);
 };
 
 export default getStylishFormatDiff;
