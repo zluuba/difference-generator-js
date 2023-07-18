b7d67d84f12149d5a113ff6009f19fe9e796512d
zluuba <zluyba.nikitina@gmail.com>
2023-07-18 10:40:23 +0300

    fix: update tests

--- __tests__/core.test.js
+++ __tests__/core.test.js
@@ -5,19 +5,27 @@ import genDiff from '../src/core.js';
 
 let json1;
 let json2;
+let yaml1;
+let yaml2;
 let expected;
 
 beforeEach(() => {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = dirname(__filename);
 
-  json1 = `${__dirname}/../__fixtures__/nested1.json`, 'utf-8';
-  json2 = `${__dirname}/../__fixtures__/nested2.json`, 'utf-8';
-  expected = fs.readFileSync(`${__dirname}/../__fixtures__/expectedNested.txt`, 'utf-8');
+  json1 = `${__dirname}/../__fixtures__/file1.json`, 'utf-8';
+  json2 = `${__dirname}/../__fixtures__/file2.json`, 'utf-8';
+  yaml1 = `${__dirname}/../__fixtures__/file1.yaml`, 'utf-8';
+  yaml2 = `${__dirname}/../__fixtures__/file2.yaml`, 'utf-8';
+  expected = fs.readFileSync(`${__dirname}/../__fixtures__/expected.txt`, 'utf-8');
 });
 
 test("gendiff main", () => {
   const receivedJson = genDiff(json1, json2, 'stylish');
+  const receivedYaml = genDiff(yaml1, yaml2, 'stylish');
+  const receivedMix = genDiff(json1, yaml2, 'stylish');
 
   expect(expected).toEqual(receivedJson);
+  expect(expected).toEqual(receivedYaml);
+  expect(expected).toEqual(receivedMix);
 });
